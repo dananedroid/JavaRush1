7a1370e18d715d6bcc2e2df69ede1291050c2e0d
Dana Kuzerova <dana.kuzerova@yandex.ru>
2021-09-28 01:07:24 +0300

    lecture 2 task 6

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Лапки", phrase = "у котиков мягенькие лапки",
+                line = "мягенькие лапки котиков бегают по дому",
+                text = "мягенькие лапки котиков бегают по дому и охотятся на людей...";
+
+    }
+}
