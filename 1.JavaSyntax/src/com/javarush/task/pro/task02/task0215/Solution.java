68f76aa2964e91f70b9e4cdb95f961ea2d0c43c5
Dana Kuzerova <dana.kuzerova@yandex.ru>
2021-10-03 20:48:06 +0300

    lecture 2 task 15

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0215/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task02.task0215;
+
+import java.util.Scanner;
+
+/* 
+Чтение чисел
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int a = c.nextInt();
+        int b = c.nextInt();
+        int e = c.nextInt();
+
+        System.out.println((a + b + e) / 3);
+
+
+
+    }
+}
