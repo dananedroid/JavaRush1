027fc578e5db454ac67b59dcf06d621eb8c87fd9
Dana Kuzerova <dana.kuzerova@yandex.ru>
2021-12-23 12:57:36 +0300

    lecture 3 task 11

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -12,9 +12,9 @@ public class Solution {
         double number1 = scanner.nextDouble();
         double number2 = scanner.nextDouble();
 
-        if (Math.abs(number2 - number1) < 0.000001)
-        System.out.println("числа равны");
-        else
+        if (Math.abs(number2 - number1) < 0.000001) {
+            System.out.println("числа равны");
+        } else
         System.out.println("числа не равны");
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -8,18 +8,9 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner console = new Scanner (System.in);
-        String text1 = console.nextLine();
-        String text2 = console.nextLine();
-        String result = text1.equals(text2) ? "Одинаковые" : "Разные";
-        System.out.println(result);
-
-        //System.out.println (text1.equals(text2));
-
-        // if (true) {
-        //System.out.println("строки одинаковые");
-         //else {
-         //   System.out.println("строки разные");
-
-        }
+        //напишите тут ваш код
+        System.out.println("строки одинаковые");
+        //напишите тут ваш код
+        System.out.println("строки разные");
+    }
 }
